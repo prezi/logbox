2c50fddedc896af2c5f11ba1c01e9031207a88c2
Zoltan Toth <zoltan.toth@prezi.com>
2013-10-03 17:20:41 +0200

    missing file added

---
+++ src/main/java/com/prezi/IgnoreUnknownParametersParser.java
@@ -0,0 +1,24 @@
+package com.prezi;
+
+import org.apache.commons.cli.BasicParser;
+import org.apache.commons.cli.ParseException;
+
+import java.util.ListIterator;
+
+public class IgnoreUnknownParametersParser extends BasicParser {
+
+    private boolean ignoreUnrecognizedOption;
+
+    public IgnoreUnknownParametersParser () {
+    }
+
+    @Override
+    protected void processOption(final String arg, final ListIterator iter) throws ParseException {
+        boolean hasOption = getOptions().hasOption(arg);
+
+        if (hasOption) {
+            super.processOption(arg, iter);
+        }
+    }
+
+}
