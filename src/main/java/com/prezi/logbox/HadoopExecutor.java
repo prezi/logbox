71346f997b34b06904b093fc60151fe8b8447a35
Julianna Gobolos-Szabo <julianna.gobolos-szabo@prezi.com>
2013-10-04 17:26:16 +0200

    Reducer is ready, no duplication in the output. After the Lzo creation indexes are created.

--- src/main/java/com/prezi/logbox/HadoopExecutor.java
+++ src/main/java/com/prezi/logbox/HadoopExecutor.java
@@ -1,5 +1,6 @@
 package com.prezi.logbox;
 
+import com.hadoop.compression.lzo.DistributedLzoIndexer;
 import com.hadoop.compression.lzo.LzopCodec;
 import com.prezi.FileUtils;
 import com.prezi.hadoop.OverwriteOutputDirTextOutputFormat;
@@ -7,6 +8,7 @@ import com.prezi.logbox.config.CategoryConfiguration;
 import com.prezi.logbox.config.ExecutionContext;
 import com.prezi.logbox.config.LogBoxConfiguration;
 import com.prezi.logbox.config.Rule;
+import com.sun.xml.internal.xsom.impl.scd.Iterators;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
@@ -17,17 +19,19 @@ import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.FileSplit;
 import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.MultipleOutputs;
 
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
 import java.io.IOException;
+
 
 
 public class HadoopExecutor extends Configured implements Tool, Executor
@@ -35,12 +39,19 @@ public class HadoopExecutor extends Configured implements Tool, Executor
     private static Log log = LogFactory.getLog(HadoopExecutor.class);
     private ExecutionContext context;
 
+    private String[] outdirList;
+    private Boolean indexing;
+
     public HadoopExecutor(ExecutionContext c) {
         this.context = c;
     }
 
     public void execute(String[] cliArgs) throws Exception {
         int res = ToolRunner.run(new Configuration(), this, cliArgs);
+
+        if (indexing) {
+            int exitCode = ToolRunner.run(new DistributedLzoIndexer(), outdirList);
+        }
     }
 
     @Override
@@ -52,15 +63,19 @@ public class HadoopExecutor extends Configured implements Tool, Executor
         Job job = new Job(conf, "logbox");
         job.setJarByClass(HadoopExecutor.class);
 
-        job.setOutputKeyClass(NullWritable.class);
-        job.setOutputValueClass(Text.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(NullWritable.class);
 
         job.setMapperClass(Map.class);
+        job.setReducerClass(Reduce.class);
+
         job.setJarByClass(HadoopExecutor.class);
 
         job.setInputFormatClass(TextInputFormat.class);
         job.setOutputFormatClass(OverwriteOutputDirTextOutputFormat.class);
+
         FileOutputFormat.setCompressOutput(job, false);
+        indexing = false;
 
         String inputLocationPrefix = context.getConfig().getInputLocationPrefix();
         log.debug("inputLocationPrefix set to: " + inputLocationPrefix);
@@ -77,11 +92,15 @@ public class HadoopExecutor extends Configured implements Tool, Executor
 
         if ( context.getConfig().getOutputCompression().equals("lzo")){
             FileOutputFormat.setCompressOutput(job, true);
+            indexing = true;
             FileOutputFormat.setOutputCompressorClass(job, LzopCodec.class);
 
             //TODO: Add dependent job for indexing
         }
 
+
+        outdirList = new String[1];
+        outdirList[0] = context.getConfig().getOutputLocationBase();
 
         FileOutputFormat.setOutputPath(job, new Path(context.getConfig().getOutputLocationBase()));
 
@@ -96,24 +115,16 @@ public class HadoopExecutor extends Configured implements Tool, Executor
         return 0;
     }
 
-    public static class Map extends Mapper<LongWritable, Text, NullWritable, Text> {
-        private MultipleOutputs<NullWritable, Text> multipleOutputs;
+    public static class Map extends Mapper<LongWritable, Text,  Text, NullWritable> {
+
         private String inputPath;
         private String inputBaseName;
         private LogBoxConfiguration config;
-        // private ArrayList<Rule> applicableRules = new ArrayList<Rule>();
 
         @Override
         protected void setup(Context context)
                 throws IOException, InterruptedException {
             inputPath = ((FileSplit) context.getInputSplit()).getPath().toString();
-            /*
-            if (inputPath.endsWith(".index")) {
-                log.info("Ignoring *.index files: " + inputPath);
-                return;
-            }
-            */
-            multipleOutputs = new MultipleOutputs<NullWritable, Text>(context);
 
             String configJSON = context.getConfiguration().get("config.json");
             config = LogBoxConfiguration.fromConfig(configJSON);
@@ -138,7 +149,8 @@ public class HadoopExecutor extends Configured implements Tool, Executor
             for (CategoryConfiguration c : config.getCategoryConfigurations()) {
                 for (Rule r : c.getRules()) {
                     if (r.matches(line)) {
-                        multipleOutputs.write(NullWritable.get(), new Text(r.getSubstitutedLine(line)), r.getSubstitutedOutputLocation(line) + "/part");
+                        Text lineAndLocation = new Text(r.getSubstitutedLine(line) + "|" + r.getSubstitutedOutputLocation(line) + "/part");
+                        context.write(lineAndLocation, NullWritable.get());
                     }
                 }
             }
@@ -147,6 +159,28 @@ public class HadoopExecutor extends Configured implements Tool, Executor
         @Override
         protected void cleanup(Context context)
                 throws IOException, InterruptedException {
+
+        }
+    }
+
+    public static class Reduce extends Reducer<Text, NullWritable, Text, NullWritable> {
+        private MultipleOutputs<Text, NullWritable> multipleOutputs;
+
+        @Override
+        protected void setup(Context context) throws IOException, InterruptedException {
+            multipleOutputs = new MultipleOutputs<Text, NullWritable>(context);
+        }
+
+        @Override
+        protected void reduce(Text key, Iterable<NullWritable> values, Context context) throws IOException, InterruptedException {
+            String[] fields = key.toString().split("\\|");
+            if (fields.length == 2) {
+                multipleOutputs.write(new Text(fields[0]), NullWritable.get(), fields[1]);
+            }
+        }
+
+        @Override
+        protected void cleanup(Context context) throws IOException, InterruptedException {
             multipleOutputs.close();
         }
     }
