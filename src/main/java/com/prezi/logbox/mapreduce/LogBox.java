72a055fc4e804dab08b21c625c972e709858338a
treff7es <tamas.nemeth@prezi.com>
2015-11-24 13:48:57 +0100

    Fixing input validation to work with s3 uri as well

--- src/main/java/com/prezi/logbox/mapreduce/LogBox.java
+++ src/main/java/com/prezi/logbox/mapreduce/LogBox.java
@@ -13,6 +13,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.s3.S3FileSystem;
 import org.apache.hadoop.io.NullWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -23,18 +24,21 @@ import org.apache.hadoop.mapreduce.lib.output.LazyOutputFormat;
 import org.apache.hadoop.util.Tool;
 
 import java.io.IOException;
+import java.net.URI;
 
 
 public class LogBox extends Configured implements Tool {
     private static Log log = LogFactory.getLog(LogBox.class);
     private ExecutionContext executionContext;
     final private int DEFAULT_NUMBER_OF_REDUCERS = 20;
+    final private String LOGBOX_VERSION = "1.1";
 
     public LogBox(ExecutionContext c) {
         this.executionContext = c;
     }
 
     private Job createJob(Configuration conf) throws IOException {
+        log.info("Starting logbox version: " + LOGBOX_VERSION );
         conf.setBoolean("mapreduce.map.tasks.speculative.execution",false);
         conf.setBoolean("mapreduce.reduce.tasks.speculative.execution",false);
         conf.setBoolean("mapred.map.tasks.speculative.execution",false);
@@ -73,18 +77,20 @@ public class LogBox extends Configured implements Tool {
         FileOutputFormat.setCompressOutput(job, false);
 
         String inputLocationPrefix = executionContext.getConfig().getInputLocationPrefix();
-        FileSystem fs = FileSystem.get(conf);
 
         for (CategoryConfiguration c : executionContext.getConfig().getCategoryConfigurations()) {
             System.out.println("=== " + inputLocationPrefix);
             String inputLocation = inputLocationPrefix + c.getInputGlob();
             System.out.println("--- " + inputLocation);
-            if (fs.exists(new Path(inputLocation))){
-                log.info("Adding input glob: " + inputLocation);
+
+            log.info("Adding input glob: " + inputLocation);
+
+            FileSystem fs = FileSystem.get(URI.create(inputLocation), job.getConfiguration());
+            if (fs.exists(new Path(inputLocation))) {
                 FileInputFormat.setInputPathFilter(job, IndexFilter.class);
                 FileInputFormat.addInputPath(job, new Path(inputLocation));
-            }else {
-                log.warn("Input does not exist: " + inputLocation);
+            } else {
+                log.info("Missing input location: " + inputLocation);
             }
         }
 
